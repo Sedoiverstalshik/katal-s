4de72ccb73f7899dbe33bcdcc3d10a78accabd83
Николай Асташев <sedoiverstalshik@mail.ru>
2023-12-26 22:31:00 +0200

    qwd

--- js/script.js
+++ js/script.js
@@ -13,9 +13,13 @@ const autoSwiper = new Swiper('.auto__swiper', {
 const workSwiper = new Swiper('.work__swiper', {
   loop: true,
 	slidesPerView: 1,
+  effect: 'fade',
 
   navigation: {
     nextEl: '.work__button-next',
     prevEl: '.work__button-prev',
+  },
+  pagination: {
+    el: '.work__swiper-pagination',
   },
 });
